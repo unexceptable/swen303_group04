08493d70fb897dadfb6fda9e0b4b1d950f77e49f
Ronni Perez <rjperez94@gmail.com>
2016-05-14 17:40:07 +1200

    Label edits and typo edits

--- MarketPlace/web_shop/views.py
+++ MarketPlace/web_shop/views.py
@@ -174,7 +174,7 @@ def logout_user(request):
 
 def edit_details(request):
     #Check login
-    if(request.user.is_authenticated):
+    if request.user.is_authenticated:
         # if this is a POST request we need to process the form data
         if request.method == 'POST':
             # create a form instance and populate it with data from the request:
